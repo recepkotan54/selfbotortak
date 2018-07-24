45b9df31e4e4bf71ef5e9f655411cfd12d42dcec
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-25 00:29:20 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const RichEmbed = new Discord.RichEmbed();
+var fs = require('fs');
+
+// -->COMMANDS<--
+
+bot.on('guildMemberAdd',member =>{
+    member.send('**Biz Büüymekte Olan Bir Discord Sunucusuyuz** https://discord.gg/Ee7hBtV');
+
+    //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
+
+});
+
+
+bot.on('guildMemberRemove', member=>{
+   
+});
+
+
+
+bot.on('ready',()=> {
+console.log('NEGRONN');
+
+});
+
+bot.login("TOKEN");
